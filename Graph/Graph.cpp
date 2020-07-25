94a919144fc153d3f4b253462fbcde772949e3dd
郭玉明 <857135447@qq.com>
2020-07-25 18:55:30 +0800

    添加邻接矩阵的深搜&广搜

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <queue>
 using namespace std;
 
 //V表示顶点类型，W表示权值类型，D表示是否有向
@@ -30,7 +31,64 @@ public:
 		if (D == false) {
 			_edges[dst_index][src_index] = w;
 		}
+	}
 
+
+	//给定顶点去深搜
+	void DFS(const V& vertex) {
+		if (_vertexIndexMap.find(vertex) == _vertexIndexMap.end()) {
+			return;
+		}
+		int srcIndex = _vertexIndexMap[vertex];
+		vector<bool> visited;
+		visited.resize(_vertexIndexMap.size(), false);
+		_DFS(srcIndex, visited, srcIndex);
+	}
+
+	void BFS(const V& vertex) {
+		if (_vertexIndexMap.find(vertex) == _vertexIndexMap.end()) {
+			return;
+		}
+		int srcIndex = _vertexIndexMap[vertex];
+		vector<bool> visited;
+		visited.resize(_vertexIndexMap.size(), false);
+		_BFS(srcIndex, visited);
+
+	}
+
+private:
+	//深度优先搜索，后面的第三个参数只是为了打印格式而传入，判断是否是第一个顶点，如果是的话但因与后面有所不同
+	void _DFS(int srcIndex, vector<bool>& visited, const int& first_vertex) {
+		visited[srcIndex] = true;
+		if (srcIndex == first_vertex) {
+			cout << _vertex[srcIndex];
+		} else {
+			cout << "->" << _vertex[srcIndex];
+		}
+		for (int i = 0; i < _edges[srcIndex].size(); ++i) {
+			if (_edges[srcIndex][i] != W() && visited[i] == false) {
+				_DFS(i, visited, first_vertex);
+			}
+		}
+	}
+
+	void _BFS(int srcIndex, vector<bool>& visited) {
+		visited[srcIndex] = true;
+		cout << _vertex[srcIndex];
+		queue<int> q;
+		q.push(srcIndex);
+
+		while (!q.empty()) {
+			int index = q.front();
+			for (int i = 0; i < _edges[index].size(); ++i) {
+				if (_edges[index][i] != W() && visited[i] == false) {
+					cout << "->" << _vertex[i];
+					visited[i] = true;
+					q.push(i);
+				}
+			}
+			q.pop();
+		}
 	}
 private:
 	int get_vertex_index(const V& vertex) {
@@ -56,7 +114,7 @@ private:
 	}
 
 private:
-	//顶点集合
+	//顶点集合,有了map，是否还要使用vector存储顶点信息？留着是有用的，深搜可以看到，如果变例输出顶点就要根据下标找顶点
 	vector<V> _vertex;
 	//邻接矩阵，边的集合
 	vector<vector<W>> _edges;
@@ -74,10 +132,17 @@ void testGraph() {
 	g.AddEdge("james", "curry", 80);
 	g.AddEdge("durant", "curry", 90);
 	g.AddEdge("durant", "tomthion", 90);
+
+	g.DFS("curry");
+	cout << endl;
+	g.BFS("curry");
+	cout << endl;
 }
 
+#if 0
 int main() {
 	testGraph();
 
 	return 0;
 }
+#endif
